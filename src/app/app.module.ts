15f1317043e59413dfae0ee7c934c737fb610374
Warllei Martins <warllei.s.martins@hotmail.com>
2021-11-28 08:35:54 -0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { FormControlComponent } from './form-control/form-control.component';
 
 @NgModule({
-  declarations: [AppComponent, FormComponent],
+  declarations: [AppComponent, FormComponent, FormControlComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/form-control/form-control.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control',
+  templateUrl: './form-control.component.html',
+  styleUrls: ['./form-control.component.css'],
+})
+export class FormControlComponent implements OnInit {
+  @Input() mostrarErro: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
